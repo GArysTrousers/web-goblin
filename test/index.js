cf28cbd45376c980835264e2a576ff9cdb488088
Ben Lee <43531407+GArysTrousers@users.noreply.github.com>
2022-08-03 18:42:50 +1000

    [add] test server

--- test/index.js
+++ test/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const http = require('http')
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end(`Hi Mom!\nPID:${process.pid}`);
+};
+
+let server = http.createServer(requestListener)
+server.listen(4000, 'localhost', () => {
+  console.log('server started on port 4000');
+})
